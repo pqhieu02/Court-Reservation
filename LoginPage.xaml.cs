f432a1582206b5df2b4d2e2f419f6294307a3281
pqhieu02 <quanghieu221002@gmail.com>
2023-04-12 19:40:42 +0300

    first commit

---
+++ LoginPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows;
+using System.Windows.Navigation;
+
+namespace Reservation_for_Sport_Courts
+{
+    public class LoginPage
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+    
+}
+
